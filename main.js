65c9c70cbc0b32d3b5736b4e3951622b6edae3fd
chae0831 <70255234+chae0831@users.noreply.github.com>
2021-02-14 23:49:47 +0900

    portfolio_REM

---
+++ main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+//navbar transparent
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+console.log(window.scrollY);
+console.log('navbarHeight: ${navbarHeight}');
+if (window.scrollY>navbarHeight) {
+    navbar.classList.add('navbar-dark');
+} else {
+    navbar.classList.remove('navbar-dark');
+}
+}); 
+
+//navbar menu scroll
+
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target=event.target;
+    const link = target.dataset.link;
+    if(link==null){
+        return;
+    }
+
+
+const scrollTo= document.querySelector(link);
+scrollTo.scrollIntoView({behavior: "smooth"});
+
+});
+
+// navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.nav_toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+navbarMenu.classList.toggle('open');
+});
+
+//home slowly fade
+
+const home=document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',() => {
+    home.style.opacity = 1-window.scrollY/homeHeight;
+});
+
+//show arrow button
+
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY>homeHeight/2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+//click on the "arrow up" button
+
+arrowUp.addEventListener('click',() => {
+    scrollIntoView('#home');
+});
+
+function scrollIntoView(selector){
+    const scrollTo= document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
